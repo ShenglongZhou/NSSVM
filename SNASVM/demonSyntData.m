221b311c4c275f469ef5858a368747d44116680b
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2020-06-18 19:35:14 +0100

    Add files via upload

---
+++ SNASVM/demonSyntData.m
@@ -0,0 +1,26 @@
+% demon randomly generated data
+clc; close all; clear all;  warning off
+ 
+type        = 2; % 1 or 2
+m0          = 2e2*(type==1)+2e6*(type~=1);
+[X,y,tX,ty] = randomData('2D',m0,2,0);  
+m           = size(X,1); 
+
+pars.s0     = ceil(log10(m)*(10*(m<=1e4)+100*(m>1e4)));
+out         = SNASVM(X,y,pars); 
+
+[acc,~,ec]  = accuracy(X,out.w,y);
+tacc        = accuracy(tX,out.w,ty);
+
+fprintf('Training  Time:             %5.3fsec\n',out.time);
+fprintf('Training  Size:             %dx%d\n',m,2);
+fprintf('Training  Accuracy:         %5.2f%%\n',out.acc*100) 
+fprintf('Testing   Size:             %dx%d\n',size(tX,1),2);
+fprintf('Testing   Accuracy:         %5.2f%%\n',tacc*100);
+fprintf('Number of Support Vectors:  %d\n',out.sv); 
+if type==1
+   figure(1),plot2D(X,y,ec,out.w,'snasvm',acc)
+   saveas(figure(1), 'output\snasvm-2D.eps','epsc');
+   saveas(figure(1), 'output\snasvm-2D.fig');
+end
+ 
---
+++ SNASVM/startup.m
@@ -0,0 +1,4 @@
+clc; close all; clear all; warning off
+addpath(genpath(pwd));
+ 
+ 
