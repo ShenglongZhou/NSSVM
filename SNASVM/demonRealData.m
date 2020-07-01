2d28fe6b145e85c0dd939e2e6fc34578bb8ca474
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2020-07-01 22:44:10 +0100

    Update demonRealData.m

--- SNASVM/demonRealData.m
+++ SNASVM/demonRealData.m
@@ -8,7 +8,7 @@ classs = load(strcat(name,'class.mat'));
 M      = size(data.X,1); classs.y(classs.y~=1)= -1;       
 data.X = normalization(data.X,2*(max(data.X(:))>1)); % normalize the data
  
-% splitting the data into training and testing data
+% randomly splitting the data into training and testing data
 m0 = ceil(0.9*M);  mt = M-m0; rng(1);   I  = randperm(M);
 Tt = I(1:mt);      tX = data.X(Tt,:);   ty = classs.y(Tt);   % testing  data 
 T  = I(1+mt:end);  X  = data.X(T,:);    y  = classs.y(T,:);  % training data
