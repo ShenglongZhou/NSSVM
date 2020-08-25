76a563ac05d237ae66a9938502bc16d1e1af24a0
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2020-08-25 19:50:17 +0100

    Update demonRealData.m

--- NSSVM/demonRealData.m
+++ NSSVM/demonRealData.m
@@ -1,23 +1,20 @@
 % demon real data data
 clc; close all; clear all; warning off
-name   = 'dhrb'; 
-data   = load(strcat(name,'.mat')); 
-class  = load(strcat(name,'class.mat')); 
-M      = size(data.X,1); class.y(class.y~=1)= -1;       
-data.X = normalization(data.X,2*(max(data.X(:))>1)); % normalize the data
+  
+load 'dhrb.mat';   load 'dhrbclass.mat'; 
+M  = size(X,1);    y(y~=1)= -1;       
+X  = normalization(X,2*(max(X(:))>1)); % normalize the data
  
-% randomly splitting the data into training and testing data
-m0 = ceil(0.9*M);  mt = M-m0; rng(1);   I  = randperm(M);
-Tt = I(1:mt);      tX = data.X(Tt,:);   ty = class.y(Tt);   % testing  data 
-T  = I(1+mt:end);  X  = data.X(T,:);    y  = class.y(T,:);  % training data
-clear data class  
+% randomly split the data into training and testing data
+m0 = ceil(0.9*M);  mt = M-m0; rng(1);  I  = randperm(M);
+Tt = I(1:mt);      tX = X(Tt,:);       ty = y(Tt);   % testing  data 
+T  = I(1+mt:end);  X  = X(T,:);        y  = y(T,:);  % training data
 
-[m,n]        = size(X); 
-out          = NSSVM(X,y); 
-tacc         = accuracy(tX,out.w,ty);
+out  = NSSVM(X,y); 
+tacc = accuracy(tX,out.w,ty);
 fprintf('Training  Time:             %5.3fsec\n',out.time);
-fprintf('Training  Size:             %dx%d\n',m,n);
+fprintf('Training  Size:             %dx%d\n',size(X,1),size(X,2));
 fprintf('Training  Accuracy:         %5.2f%%\n',out.acc*100);
 fprintf('Number of Support Vectors:  %d\n',out.sv); 
-fprintf('Testing   Size:             %dx%d\n',size(tX,1),n);
+fprintf('Testing   Size:             %dx%d\n',size(tX,1),size(tX,2));
 fprintf('Testing   Accuracy:         %5.2f%%\n',tacc*100);
